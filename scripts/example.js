266c40b15d9a1a1baf67adeaec6aab36c199a17e
Matthieu SCARSET <m@matthieuscarset.com>
2022-02-13 16:00:15 +0100

    Fix typo in example

--- scripts/example.js
+++ scripts/example.js
@@ -6,20 +6,15 @@
  *
  * @see https://trufflesuite.com/docs/truffle/getting-started/writing-external-scripts.html
  */
-const myContracts = ['MyFriends', 'MyNFT'];
-
 module.exports = async function main(callback) {
   try {
-      // Get deployed contract instance.
-      // Don't forget to run `truffle migrate` before.
-      const Contract = artifacts.require('MyFriends');
-      const instance = await Contract.deployed();
+    // Get deployed contract instance.
+    // Don't forget to run `truffle migrate` before.
+    let Contract = artifacts.require('MyFriends');
+    let instance = await Contract.deployed();
 
-      // Ex: check contract's owner.
-      await instance.owner().then(address => console.log(address));
-
-    myContracts.forEach((name) => {
-    });
+    // Ex: check contract's owner.
+    await instance.owner().then(address => console.log(address));
 
     callback(0);
   } catch (error) {
