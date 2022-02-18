95b23048415f5887d78a7cf0abb53838405c85c5
Matthieu SCARSET <m@matthieuscarset.com>
2022-02-18 17:51:37 +0100

    use default 8545 port

--- truffle-config.js
+++ truffle-config.js
@@ -2,7 +2,7 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 8545,
       network_id: "*",
     },
   },
@@ -10,7 +10,7 @@ module.exports = {
   },
   compilers: {
     solc: {
-      version: "0.8.11",
+      version: "0.7.0",
     }
   },
 };
